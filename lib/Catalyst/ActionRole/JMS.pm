364cb56e1f3f941acd59b7a4ef6b62f5e6d85794
Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
2011-12-12 15:17:19 +0000

    first stab

--- lib/Catalyst/ActionRole/JMS.pm
+++ lib/Catalyst/ActionRole/JMS.pm
@@ -1,5 +1,42 @@
+package Catalyst::ActionRole::JMS;
 use strict;
 use warnings;
-package Catalyst::ActionRole::JMS;
+use Moose::Role;
+use List::Util 'first';
+use namespace::autoclean;
+
+requires 'attributes';
+
+has jmstype => (
+    is => 'ro',
+    isa => 'Str',
+    required => 1,
+);
+
+sub _extract_jmstype {
+    my ($self,$ctx) = @_;
+
+    my $ret = $ctx->request->headers->header('JMSType');
+    return $ret if defined $ret;
+    my $env = $ctx->engine->env;
+    my $key = first { /\.jmstype$/ } keys %{$env};
+    return $env->{$key};
+}
+
+sub _match_jmstype {
+    my ($self,$req_jmstype) = @_;
+
+    return $self->jmstype eq $req_jmstype;
+}
+
+around match => sub {
+    my ($orig,$self,$ctx) = @_;
+
+    my $req_jmstype = $self->_extract_jmstype($ctx);
+    if ($self->_match_jmstype($req_jmstype)) {
+        return $self->$orig($ctx);
+    }
+    return 0;
+};
 
 1;
