a81438dbc9ff5b13422c9a2d0dd25d96612c447e
Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
2014-07-29 10:54:02 +0100

    Dzil-build release 1.3 (from f0b0ad3 on master)

--- lib/Catalyst/ActionRole/JMS.pm
+++ lib/Catalyst/ActionRole/JMS.pm
@@ -1,4 +1,8 @@
 package Catalyst::ActionRole::JMS;
+$Catalyst::ActionRole::JMS::VERSION = '1.3';
+{
+  $Catalyst::ActionRole::JMS::DIST = 'Catalyst-ActionRole-JMS';
+}
 use strict;
 use warnings;
 use Moose::Role;
@@ -6,6 +10,73 @@ use List::Util 'first';
 use namespace::autoclean;
 
 # ABSTRACT: role for actions to dispatch based on JMSType
+
+
+requires 'attributes';
+
+
+has jmstype => (
+    is => 'ro',
+    isa => 'Str',
+    lazy => 1,
+    builder => '_build_jmstype',
+);
+
+sub _build_jmstype {
+    my ($self) = @_;
+
+    return $self->attributes->{JMSType}[0] || $self->name;
+}
+
+
+around match => sub {
+    my ($orig,$self,$ctx) = @_;
+
+    # ugly hack, some pieces along the way lose the method
+    $ctx->req->method('POST') unless $ctx->req->method;
+
+    my $req_jmstype = $self->_extract_jmstype($ctx);
+    if ($self->_match_jmstype($req_jmstype)) {
+        return $self->$orig($ctx);
+    }
+    return 0;
+};
+
+
+sub _extract_jmstype {
+    my ($self,$ctx) = @_;
+
+    my $ret = $ctx->request->headers->header('jmstype')
+        || $ctx->request->headers->header('type');
+    return $ret if defined $ret;
+    my $env = eval { $ctx->engine->env } || $ctx->request->env;
+
+    return $env->{'jms.type'};
+}
+
+
+sub _match_jmstype {
+    my ($self,$req_jmstype) = @_;
+
+    return $self->jmstype eq $req_jmstype;
+}
+
+
+1;
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Catalyst::ActionRole::JMS - role for actions to dispatch based on JMSType
+
+=head1 VERSION
+
+version 1.3
 
 =head1 SYNOPSIS
 
@@ -25,31 +96,16 @@ should be dispatched to the namespace of the action.
 You should look at L<Catalyst::Controller::JMS> for a more integrated
 solution using this module together with automatic (de-)serialization.
 
-=cut
+=head1 ATTRIBUTES
 
-requires 'attributes';
-
-=attr C<jmstype>
+=head2 C<jmstype>
 
 The type to match against. Defaults to the value of a C<JMSType>
 action attribute, or the action name if such attribute is not present.
 
-=cut
+=head1 METHODS
 
-has jmstype => (
-    is => 'ro',
-    isa => 'Str',
-    lazy => 1,
-    builder => '_build_jmstype',
-);
-
-sub _build_jmstype {
-    my ($self) = @_;
-
-    return $self->attributes->{JMSType}[0] || $self->name;
-}
-
-=method C<match>
+=head2 C<match>
 
 C<around> modifier for the C<match> method of the action
 class. Extracts the request / message type by calling
@@ -58,22 +114,7 @@ L</jmstype> attribute by calling L</_match_jmstype>. If it matches,
 delegates to the normal C<match> method, otherwise signals a non-match
 to the dispatched by returning false.
 
-=cut
-
-around match => sub {
-    my ($orig,$self,$ctx) = @_;
-
-    # ugly hack, some pieces along the way lose the method
-    $ctx->req->method('POST') unless $ctx->req->method;
-
-    my $req_jmstype = $self->_extract_jmstype($ctx);
-    if ($self->_match_jmstype($req_jmstype)) {
-        return $self->$orig($ctx);
-    }
-    return 0;
-};
-
-=method C<_extract_jmstype>
+=head2 C<_extract_jmstype>
 
   my $type = $self->_extract_jmstype($ctx);
 
@@ -81,39 +122,27 @@ Gets the type of the request / message. It first looks in the request
 headers for C<jmstype> or C<type> keys, then looks into the PSGI
 environment for a C<jms.type> key.
 
-=cut
-
-sub _extract_jmstype {
-    my ($self,$ctx) = @_;
-
-    my $ret = $ctx->request->headers->header('jmstype')
-        || $ctx->request->headers->header('type');
-    return $ret if defined $ret;
-    my $env = eval { $ctx->engine->env } || $ctx->request->env;
-
-    return $env->{'jms.type'};
-}
-
-=method C<_match_jmstype>
+=head2 C<_match_jmstype>
 
   my $ok = $self->_match_jmstype($request_type);
 
 Simple string equality comparison. Override this if you need more
 complicated matching semantics.
 
-=cut
-
-sub _match_jmstype {
-    my ($self,$req_jmstype) = @_;
-
-    return $self->jmstype eq $req_jmstype;
-}
-
 =head1 EXAMPLES
 
 You can find examples of use in the tests, or at
 https://github.com/dakkar/CatalystX-StompSampleApps
 
-=cut
+=head1 AUTHOR
 
-1;
+Gianni Ceccarelli <gianni.ceccarelli@net-a-porter.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Net-a-porter.com.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
